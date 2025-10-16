40494f13ac2925ecbf98b5b658e032b06d0422d4
JoiceGloriya <joicegloriyaa@gmail.com>
2025-10-16 22:12:13 +0530

    Initial commit for React frontend

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appWrite";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`, //this verifies, who' s making request
+  },
+};
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); //to show Loader
+  const [debouncedSearchTerm, setDebouncedSerachTerm] = useState("");
+
+  useDebounce(() => setDebouncedSerachTerm(searchTerm), 500, [searchTerm]);//for optimised searching
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) throw new Error();
+
+      const data = await response.json();
+      if (data.Response === "false") {
+        setErrorMessage(data.Error || `Error fetching movies`);
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+      updateSearchCount()
+    } catch (error) {
+      setErrorMessage(`Error fetching movies`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]); //whenvr user searches somethng, then gets invoked
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> you' ll enjoy
+            without the hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>{" "}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/appWrite.js
@@ -0,0 +1,7 @@
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DB_ID = import.meta.env.VITE_APPWRITE_DB_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+export const updateSearchCount = async () => {
+  
+};
